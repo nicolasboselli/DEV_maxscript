b43f897362dc82c3036e7d5aec1c9d9401a773fb
nicolasboselli <boselli.nico@gmail.com>
2017-12-20 17:20:02 +0100

    visiolab to master

--- 00_wip/python_test/test.py
+++ 00_wip/python_test/test.py
@@ -1,14 +1,17 @@
-
-import pymxs
+from pprint import pprint
+import pymxs as pm
 import MaxPlus
 
 import pymxs
 
-rt = pymxsExtend.runtime
-
 rt = pymxs.runtime
 
-rt.
-#~ rt.rollout(rlt,"toto")
 
 
+b = rt.box()
+pprint(b.width)
+print b.width
+
+print  "size: %(n)04d" % {"n" : 5}
+print '%(language)s has %(number)03d quote types.' %  {"language": "Python", "number": 2}
+	
